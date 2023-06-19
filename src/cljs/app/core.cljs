f6e89821bc76a754a424325ebd672afe04b72322
Jacob Doran <j.doran.admin@gmail.com>
2023-06-19 19:14:07 +0800

    Added lucide-react dependency

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,15 +1,17 @@
 (ns app.core
   (:require
    ["react-dom/client" :as rdom]
+   ["lucide-react" :refer [BookMarked]]
    [helix.core :refer [$]]
    [helix.dom :as d])
   (:require-macros
    [app.lib :as l]))
 
 (l/defnc app []
-  (d/h1
-   {:class-name "text-red-400"}
-   "hellope"))
+  (d/div (d/h1
+          {:class-name "text-red-400"}
+          "hellope")
+         ($ BookMarked)))
 
 (defn ^:export init []
   (defonce root (rdom/createRoot (js/document.getElementById "app")))
