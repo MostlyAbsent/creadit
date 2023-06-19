df265bcf9935cb6174a5fb24da16db0262bf9f59
Jacob Doran <j.doran.admin@gmail.com>
2023-06-19 16:50:42 +0800

    Set dev enviroment
    Including shadow-cljs and tailwind hot reloading

---
+++ src/cljs/app/core.cljs
@@ -0,0 +1,16 @@
+(ns app.core
+  (:require
+   ["react-dom/client" :as rdom]
+   [helix.core :refer [$]]
+   [helix.dom :as d])
+  (:require-macros
+   [app.lib :as l]))
+
+(l/defnc app []
+  (d/h1
+   {:class-name "text-sky-300"}
+   "hellope"))
+
+(defn ^:export init []
+  (defonce root (rdom/createRoot (js/document.getElementById "app")))
+  (.render root ($ app)))
---
+++ src/cljs/app/dev.cljs
@@ -0,0 +1,8 @@
+(ns app.dev
+  (:require
+   [helix.experimental.refresh :as r]))
+
+(r/inject-hook!)
+
+(defn ^:dev/after-load refresh []
+  (r/refresh!))
