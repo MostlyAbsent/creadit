7944ea7a113dbeabd7c8e4cc6ad08dea36bd5b2e
Jacob Doran <j.doran.admin@gmail.com>
2023-06-27 18:24:07 +0800

    Configure basic front-end routing

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,18 +1,43 @@
 (ns app.core
   (:require
+   ["react" :as r]
    ["react-dom/client" :as rdom]
-   ["lucide-react" :refer [BookMarked]]
-   [helix.core :refer [$]]
+   [components.navbar :refer [navbar]]
+   [helix.core :refer [$ provider]]
+   [reitit.frontend :as rf]
+   [helix.hooks :as h]
+   [reitit.frontend.easy :as rfe]
+   [reitit.coercion.spec :as rss]
    [helix.dom :as d])
   (:require-macros
    [app.lib :as l]))
 
+(def match (atom nil))
+
+(l/defnc home []
+  (d/div "home"))
+
 (l/defnc app []
-  (d/div (d/h1
-          {:class-name "text-red-400"}
-          "hellope")
-         ($ BookMarked)))
+  (d/div
+   {:class-name "bg-white text-slate-900 antialiased light"}
+   ($ navbar)
+   (d/div
+    {:class-name "min-h-screen pt-12 bg-slate-50 antialiased"}
+    (d/div
+     {:class-name "container max-w-7xl h-full pt-12"}
+     (if @match
+       (let [view (:view (:data @match))]
+         ($ view)))))))
+
+(def routes
+  [["/"
+    {:name ::homepage
+     :view home}]])
 
 (defn ^:export init []
+  (rfe/start!
+   (rf/router routes {:data {:coercion rss/coercion}})
+   (fn [m] (reset! match m))
+   {:use-fragment true})
   (defonce root (rdom/createRoot (js/document.getElementById "app")))
   (.render root ($ app)))
